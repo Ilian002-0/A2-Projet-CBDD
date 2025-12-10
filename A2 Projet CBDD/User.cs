aed3600db8be1e49e23ceb6bb1bdabe08343354f
Ilian THRO <ilian.thro@edu.devinci.fr>
2025-12-10 11:27:36 +0100

    modif

---
+++ A2 Projet CBDD/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace A2_Projet_CBDD
+{
+    internal class User
+    {
+        string identifiant;
+        string motDePasse;
+    }
+}
